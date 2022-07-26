f7c75b746b3bc13250fe0c301e088258e2616f55
Dani Rodríguez <dani@danirod.es>
2022-07-26 20:26:30 +0200

    seo: enhance the copy with the about section

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -3,8 +3,28 @@
 	<p>
 		makigas EARLY es una forma de acceder al contenido que subo a YouTube antes de que esté en
 		abierto. Con esto agradezco la participación a los mecenas que ayudan a que este proyecto siga
-		siendo viable. Con tu suscripción a mi canal de Twitch puedes obtener acceso a los vídeos
-		mientras están en producción para que los veas antes que nadie.
+		siendo viable. Con tu suscripción a <a href="https://twitch.tv/danirod_">mi canal de Twitch</a> puedes
+		obtener acceso a los vídeos mientras están en producción para que los veas antes que nadie.
+	</p>
+
+	<p>
+		En este momento para ver los vídeos necesitarás tener la sesión de Twitch iniciada, dado que los
+		videos están alojados ahí y están configurados para sólo ser reproducibles por suscriptores. Si
+		no tienes una suscripción activa, la pantalla se podría quedar en negro. Si te suscribiste y aun
+		así no te carga, contacta conmigo por susurro de Twitch, trataré de darte algún tipo de
+		solución. En el futuro, trabajaré para que esto sea más abierto (alojar los vídeos por mi
+		cuenta, tal vez configurar formas alternativas que no sean Twitch...)
+	</p>
+
+	<p>
+		<strong>¿Significa esto que este contenido no es gratis?</strong> No. Si no quieres suscribirte,
+		esto te vale como adelanto de los proyectos en los que estoy trabajando y que están casi tan listos
+		que pronto estarán en YouTube. Cuando el vídeo se sube a YouTube, se hace abierto y gratis para que
+		todo el mundo lo pueda ver.
+	</p>
+	<p>
+		Este sistema está en pruebas y todavía la calidad es un poco regular, pero es mejor que nada y
+		me permite agradeceros el apoyo directo que me aportáis si os gusta mi contenido de Twitch. 🤗
 	</p>
 </div>
 
@@ -17,5 +37,9 @@
 		line-height: 1.5;
 		max-width: 70ch;
 		margin: 0 auto;
+	}
+
+	p + p {
+		margin-top: 1rem;
 	}
 </style>
