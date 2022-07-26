7954e594387d998f5023a8c273e32221d9c53371
Dani Rodríguez <dani@danirod.es>
2022-07-26 20:22:22 +0200

    seo: add twitter card and opengraph tags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -28,6 +28,23 @@
 
 <svelte:head>
 	<title>makigas EARLY</title>
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@makigas" />
+	<meta name="twitter:title" content="makigas EARLY" />
+	<meta
+		name="twitter:description"
+		content="Accede anticipadamente al contenido a medida que lo voy fabricando, antes de que llegue a YouTube"
+	/>
+	<meta name="twitter:image" content="https://early.makigas.es/card.jpg" />
+	<meta property="og:url" content="https://early.makigas.es" />
+	<meta property="og:title" content="makigas EARLY" />
+	<meta property="og:type" content="website" />
+	<meta
+		property="og:description"
+		content="Accede anticipadamente al contenido a medida que lo voy fabricando, antes de que llegue a YouTube"
+	/>
+	<meta property="og:site_name" content="makigas" />
+	<meta property="og:image" content="https://early.makigas.es/card.jpg" />
 </svelte:head>
 
 <Navbar />
